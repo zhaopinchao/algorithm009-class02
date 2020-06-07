06edfe4203c1fd55edb9a08009a0411adb9ca2fa
zhaopc <zhaopc@tuya.com>
2020-06-07 22:24:37 +0800

    week03 作业

---
+++ Week_03/LowestCommonAncestor.java
@@ -0,0 +1,30 @@
+/**
+ * @author: zhaopc@tuya.com
+ * @date: 2020-06-07 21:47
+ * @description 给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
+ */
+public class LowestCommonAncestor {
+
+    /**
+     * 递归方式
+     *
+     * @param root
+     * @param p
+     * @param q
+     * @return
+     */
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null) {
+            return left;
+        }
+        if (right != null) {
+            return right;
+        }
+        return root;
+    }
+}
