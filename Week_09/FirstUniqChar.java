2d736ecf4278d63a628111c328b2103259e692eb
zhaopinchao <pinchaozhao@gmail.com>
2020-07-19 20:53:40 +0800

    week09 作业

---
+++ Week_09/FirstUniqChar.java
@@ -0,0 +1,19 @@
+/**
+ * @author: zhaopc
+ * @date: 2020-07-19 18:13
+ * @description 给定一个字符串，找到它的第一个不重复的字符，并返回它的索引。如果不存在，则返回 -1。
+ */
+public class FirstUniqChar {
+
+    public int firstUniqChar(String s) {
+        if (s.length() == 0) return -1;
+        int[] countArray = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            countArray[s.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (countArray[s.charAt(i) - 'a'] == 1) return i;
+        }
+        return -1;
+    }
+}
